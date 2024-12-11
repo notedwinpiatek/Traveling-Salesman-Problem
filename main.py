21471e6ad8125f4d34379e2673e055b0e812bffa
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2024-12-11 18:09:59 +0100

    feat - print lists

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ def parse_tsp(file_path):
             parts = line.strip().split()
             if len(parts) == 3:
                 order_number.append(int(parts[0]))
-                x_cord.append(int(parts[1]))
-                y_cord.append(int(parts[2]))
-    return
+                x_cord.append(float(parts[1]))
+                y_cord.append(float(parts[2]))
+    return order_number, x_cord, y_cord
 
 
 
 
 
-parse_tsp(file_path)
+# parse_tsp(file_path)
+id_num, x, y = parse_tsp(file_path)
+print(id_num)
+print(y)
+print(x)
