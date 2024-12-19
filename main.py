a75c37ca6d4a19d9929ef3461f81482119c9c6ee
Edwin Piatek (Nokia) <edwin.piatek@nokia.com>
2024-12-19 19:23:10 +0100

    feat: greedy algoritm

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import os
 import math
 import random
 
-file_path = os.path.join(os.path.dirname(__file__), "files/berlin11_modified.tsp")
-start_line = "NODE_COORD_SECTION\n"
-end_line = "EOF\n"
+# Configuration
+start_point = 2
+file_name = input("Enter file name: ")
+
 # functions
-def parse_tsp(file_path, start_line, end_line):
+def parse_tsp(file_path):
+    start_line = "NODE_COORD_SECTION\n"
+    end_line = "EOF\n"
     order_number = []
     x_cord = []
     y_cord = []
@@ -33,16 +36,16 @@ def calculate_distance(x1, y1, x2, y2):
     return result
 
 def random_solution(id_num):
-    solution = list(range(len(id_num)))
+    solution = id_num.copy()
     random.shuffle(solution)
     solution.append(solution[0])
     return solution 
 
 def fitness(order, x, y):
     total_distance = 0
-    for i in range(len(order) - 2):  # Ensure i + 1 is valid
-        x1, y1 = x[order[i]], y[order[i]]
-        x2, y2 = x[order[i + 1]], y[order[i + 1]]
+    for i in range(len(order) - 1):
+        x1, y1 = x[order[i] - 1], y[order[i] - 1]
+        x2, y2 = x[order[i + 1] - 1], y[order[i + 1] - 1]
         total_distance += calculate_distance(x1, y1, x2, y2)
     return total_distance
 
@@ -51,26 +54,31 @@ def info(solution):
     return res
 
 def greedy(start):
-    pass
-    # identify the first element in solution
-    # for every element of your list exept for first one
-        # find the distance between first element and the one in the loop
-        # keep track of the smallest distance
-    # add distance calculated to the total distance
-    # shift your "first element" to the one that goes next
-    # repeat the process of looping throung every elemnt except for "current one"
+    solution = [start]
+    while len(solution) != len(id_num):
+        next_city = -1
+        best_distance = float("inf")
+        for city in id_num:
+            if city not in solution:
+                distance = calculate_distance(x[solution[-1]-1], y[solution[-1]-1], x[city-1], y[city-1])
+                if distance < best_distance:
+                    best_distance = distance
+                    next_city = city
+        solution.append(next_city)
+    solution.append(start)
+    return solution
     
 #executing functions 
-id_num, x, y = parse_tsp(file_path, start_line, end_line)
-distance = calculate_distance(x[0], y[0], x[1], y[1])
+file_path = os.path.join(os.path.dirname(__file__), f"files/{file_name}.tsp")
+id_num, x, y = parse_tsp(file_path)
 random_number = random_solution(id_num)
-total_distance = fitness(random_number, x, y)
+greedy_path = greedy(start_point)
+
 
 
 # printing
-print(f"Ordering number: {id_num}")
-print(f"x: {x}")
-print(f"y: {y}")
-print(f"Distance: {round(distance, 3)}")
-print(f"Random number: {info(random_number)}")
-print(f"Total distance: {round(total_distance, 3)}")
+print(f"Greedy Path: {info(greedy_path)}")
+# print(random_number)
+# print(greedy_path)
+# print(f"Random Distance: {round(fitness(random_number, x, y), 2)}")
+print(f"Greedy Distance: {round(fitness(greedy_path, x, y), 2)}")
